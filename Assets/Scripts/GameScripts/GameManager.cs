b45792d2c95b97ccebac60496c17ad5365412669
Jasper Schwinghammer <jasper@schwinghammer.de>
2022-01-16 15:53:19 +0100

    Hiding Cursor

--- Assets/Scripts/GameScripts/GameManager.cs
+++ Assets/Scripts/GameScripts/GameManager.cs
@@ -7,12 +7,22 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChangeToPlayMode();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ChangeToPlayMode()
+    {
+        Cursor.visible = false;
+    }
+
+    void ChangeToMenuMode()
+    {
+        Cursor.visible = true;
     }
 }
